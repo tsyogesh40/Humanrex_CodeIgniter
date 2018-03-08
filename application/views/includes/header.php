31fd8dbf1c4f2b92645c6a892437a13458085d4e
tsyogesh40 <tsyogesh40@gmail.com>
2018-03-08 22:22:23 +0530

    scroll bar bug

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -104,8 +104,9 @@
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu" style="height:120vh;overflow-y:scroll auto ;">
+          <!-- sidebar menu: : style can be found in sidebar.less
+          style="height:120vh;overflow-y:scroll auto;" -->
+          <ul class="sidebar-menu" >
             <li class="header">MAIN NAVIGATION</li>
             <li class="treeview">
               <a href="<?php echo base_url(); ?>dashboard">
